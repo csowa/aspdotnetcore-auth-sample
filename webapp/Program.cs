c87d85c2e839b2cd127bc25b9b8b067e42e0f6e7
Chris Sowa <csowa@resdat.com>
2023-11-17 17:43:59 -0900

    Add initialization of WsFederation configuration to "resolve" issue.

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.WsFederation;
 using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.WsFederation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +34,9 @@ builder.Services.AddAuthentication(sharedOptions =>
         // PS C:\Windows\system32> (Get-Item C:\Windows\ADFS\Microsoft.IdentityServer.ServiceHost.exe).VersionInfo.ProductVersion 
         // 10.0.17763.4644
 
+        // Initialize config to "resolve" issue:
+        SetupTokenValidationParameters(options);
+
     })
     .AddCookie();
 
@@ -60,3 +65,28 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+/// <summary>
+/// Add config manager and valid issuers from config.
+/// *** NOT production code. ***
+/// </summary>
+/// <remarks>
+/// See https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/issues/2406;
+/// See <see cref="Microsoft.AspNetCore.Authentication.WsFederation.WsFederationHandler.SetupTokenValidationParametersAsync()"/> 
+/// Verifying missing configuration / initialization as a possible cause.
+/// </remarks>
+static async void SetupTokenValidationParameters(WsFederationOptions options)
+{
+    // Operate on params themselves for this (one-time) initialization.
+    var tokenValidationParameters = options.TokenValidationParameters;
+
+    // ConfigurationManager not present at point this is called, so add here, *** with metadata configuration set in caller ***.
+    tokenValidationParameters.ConfigurationManager = new ConfigurationManager<WsFederationConfiguration>(options.MetadataAddress, new WsFederationConfigurationRetriever());
+
+    var _configuration = await tokenValidationParameters.ConfigurationManager.GetBaseConfigurationAsync(CancellationToken.None);
+
+    var issuers = new[] { _configuration.Issuer };
+
+    tokenValidationParameters.ValidIssuers = tokenValidationParameters.ValidIssuers == null ? issuers : tokenValidationParameters.ValidIssuers.Concat(issuers);
+    tokenValidationParameters.IssuerSigningKeys = tokenValidationParameters.IssuerSigningKeys == null ? _configuration.SigningKeys : tokenValidationParameters.IssuerSigningKeys.Concat(_configuration.SigningKeys);
+}
